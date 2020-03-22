14cd39ee9b3a3769084da00457f125e0dcea3567
aliyucelgamli <y_gamli@msn.com>
2020-03-22 11:36:32 +0300

    Added new game

---
+++ crossyRoad/Assets/Scripts/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+
+
+}
---
+++ crossyRoad/Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject vehicle;
+    [SerializeField] private Transform spawnPos;
+    [SerializeField] private float minSeparationTime;
+    [SerializeField] private float maxSeparationTime;
+    [SerializeField] private bool isRightSide;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnVehicle());
+    }
+
+    private IEnumerator SpawnVehicle()
+    {
+        while (true)
+        {
+        yield return new WaitForSeconds(Random.Range(minSeparationTime,maxSeparationTime));
+        GameObject go = Instantiate(vehicle, spawnPos.position, Quaternion.identity);
+
+            if(!isRightSide)
+            {
+                go.transform.Rotate(new Vector3(0, 180, 0));
+            }
+
+        }
+    }
+
+}
