f1a1e31023c0a20ecbf47771316b982d26f23fa9
aliyucelgamli <y_gamli@msn.com>
2020-03-24 18:15:55 +0300

    Shooter 2D game

---
+++ sicakCizgi/Assets/SCRIPTS/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;    
+    public int damage = 40;
+
+    public GameObject hitEffect;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+
+   
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+
+
+
+
+    /*
+     
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1f);
+        Destroy(gameObject);
+
+       
+    }
+    */
+}
